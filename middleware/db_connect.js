a435fd10b23e9fcaf3eafc027e0c521d9a6ddda0
juvencio08 <juvenc.cruz@gmail.com>
2023-05-30 13:45:16 +0700

    uopdate

--- middleware/db_connect.js
+++ middleware/db_connect.js
@@ -3,9 +3,9 @@ const mysql = require('mysql')
 // Connect database
 const connection = mysql.createConnection({
   host: 'process.evn.DB_Host',
-  user: 'process.evn.DB',
-  password: 'process.evn.DB',
-  database: 'process.evn.DB'
+  user: 'process.evn.DB_USER',
+  password: 'process.evn.DB_PASSWORD',
+  database: 'process.evn.DB_NAME',
 })
 
 module.exports = connection
